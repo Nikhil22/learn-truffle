6b71f0793a773ba17fd719f8a50bbfce1beb20a0
Nikhil22 <nikhil38@gmail.com>
2018-05-30 16:12:09 +0200

    edit chapter3

--- chapter4/truffle.js
+++ chapter4/truffle.js
@@ -12,14 +12,12 @@ module.exports = {
       network_id: 3,
       host: "127.0.0.1",
       port:  8545,
-      from: "0xd35db5027107f222566894caaf987e9051269ece",
       gas: 4612388
     },
-    rinkeby: {
+    kovan: {
       host: "127.0.0.1",
       port: 8545,
-      from: "0xd35db5027107f222566894caaf987e9051269ece",
-      network_id: 4,
+      network_id: 42,
       gas: 4612388
     }
   }
